c5e41b0d7751bc8d21a9f100cc3c16c8b1e78a64
awbalessa <azizalessa321@gmail.com>
2025-07-03 23:45:12 +0300

    Almost finalize SQLite parser

--- scripts/sql-parser/main.py
+++ scripts/sql-parser/main.py
@@ -21,28 +21,33 @@ ayat_span_block = re.compile(
     pattern=
     r"""[\s\n]*
     <span[^>]*>
-    \s*
+    [\s\n]*
     (?P<ayat_text>.*?)
-    \s*
+    [\s\n]*
     </span>
     [\s\n]*
     (?:\s*\n*\[(?P<ayat_num>.*?)\])?""",
-    flags=re.VERBOSE | re.DOTALL
+    flags=re.VERBOSE
 )
 ayat_block = re.compile(
     pattern=r"\uFD3F(?P<ayat_text>.*?)\uFD3E"
 )
 tuple_start = re.compile(
-    pattern=r"^\('"
+    pattern=r"^\('(?P<ayah_keys>.*?)',\s'"
 )
 tuple_end = re.compile(
-    pattern=r"',\)$"
-)
-bolded_block = re.compile(
-    pattern=r"\*\*.*?\*\*"
+    pattern=r"'\)$"
 )
 heading_block = re.compile(
-    pattern=r"[\s\n]*<"
+    pattern=
+    r"""
+    [\s\n]*<h\d>
+    \s*
+    (?P<heading>.*?)
+    \s*
+    </h\d>[\s\n]*
+    """,
+    flags=re.VERBOSE
 )
 
 def strip_diacritics(text: str) -> str:
@@ -50,54 +55,52 @@ def strip_diacritics(text: str) -> str:
 
 def strip_line_diacritics(text: str) -> str:
     final: List[str] = []
-    for line in text.split("\n"):
-        if bolded_block.search(line):
-            final.append(line)
+    for part in re.split(pattern=r"\*\*", string=text):
+        match = ayat_block.search(part)
+        if match:
+            final.append(part)
             continue
-        final.append(strip_diacritics(line))
-    return "\n".join(final).strip()
+        final.append(strip_diacritics(part))
+    return "".join(final).strip()
 
 def replace_ayah_span(match: Match[str]) -> str:
     ayat_text = match.group("ayat_text").strip()
     ayat_num = match.group("ayat_num")
     if ayat_num is None:
-        return f" {ayat_text} "
+        return f" **{ayat_text}** "
     else:
         ayat_num = ayat_num.strip()
         ayat_num = strip_diacritics(ayat_num)
-    return f" {ayat_text} [{ayat_num}]"
+    return f" **{ayat_text}** [{ayat_num}]"
 
 def preprocess(text: str) -> str:
     text = re.sub(inline_annotations, '', text)
-    text = re.sub(div_blocks, '', text)
-    text = re.sub(opening_paragraph_blocks, '\n', text)
-    text = re.sub(closing_paragraph_blocks, '', text)
+    text = re.sub(div_blocks, f'\n\n' , text)
+    text = re.sub(opening_paragraph_blocks, f'\n\n', text)
+    text = re.sub(closing_paragraph_blocks, f'\n\n', text)
     text = re.sub(tuple_start, '', text)
     text = re.sub(tuple_end, '', text)
+    text = re.sub(heading_block, '', text)
     text = ayat_span_block.sub(replace_ayah_span, text)
     text = strip_line_diacritics(text)
     return text
 
-CURSOR.execute("""
-    SELECT text FROM tafsir
+limit: int = 10
+offset: int = 10
+CURSOR.execute(f"""
+    SELECT ayah_keys, text FROM tafsir
     WHERE TRIM(text) != ''
-        AND (
-            ayah_key = '1:1' OR
-            ayah_key = '1:2' OR
-            ayah_key = '1:3' OR
-            ayah_key = '1:4' OR
-            ayah_key = '1:5' OR
-            ayah_key = '1:6' OR
-            ayah_key = '1:7'
-        )
+    LIMIT {limit}
+    OFFSET {offset};
     """)
-results = CURSOR.fetchall()
-for i ,row in enumerate(results):
-    soup = BeautifulSoup(str(row), 'html.parser')
-    pretty = soup.prettify()
-    # print(pretty)
+results = CURSOR.fetchmany(size=limit)
+for i, row in enumerate(results):
+    ayah_keys = row[0]
+    text_soup = BeautifulSoup(str(row[1]), 'html.parser')
+    text_pretty = str(text_soup.prettify())
     clean = preprocess(
-        text=str(pretty)
+        text=text_pretty
     )
-    print(f"\n#{i+1}\n")
+    print(f"Tafsir for {ayah_keys}\n")
     print(clean)
+    print(f"\n")
